57a9c18404ca6a979b5b7b83e15309044621c82d
bpbarwick <bpbarwick@gmail.com>
2025-03-08 15:05:37 -0500

    Quartz sync: Mar 8, 2025, 3:05 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'bpbarwick/ref_daria',
+        // from data-repo-id
+        repoId: 'R_kgDOOFWxbA',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDOOFWxbM4CnuMd',
+      }
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
