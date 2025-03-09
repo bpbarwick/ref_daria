b7267c2796290a9ed1cb7236b7d1f7ad643b21a5
bpbarwick <bpbarwick@gmail.com>
2025-03-09 17:29:05 -0400

    Quartz sync: Mar 9, 2025, 5:29 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -77,6 +77,7 @@ const config: QuartzConfig = {
       Plugin.AliasRedirects(),
       Plugin.ComponentResources(),
       Plugin.ContentPage(),
+      Plugin.CreatedModifiedDate(),
       Plugin.FolderPage(),
       Plugin.TagPage(),
       Plugin.ContentIndex({
